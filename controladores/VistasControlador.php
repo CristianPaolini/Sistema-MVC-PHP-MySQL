227c878f47d21cedc7b59f73caa1de5483bfcb14
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-05 17:51:29 -0300

    Agregados VistasControlador y VistasModelo con algunos métodos.

---
+++ controladores/VistasControlador.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once "../modelos/VistasModelo.php";
+
+    class vistasControlador extends vistasModelo {
+
+        /*---------- Controlador obtener plantilla ----------*/
+        public function obtener_plantilla_controlador() {
+            return require_once "../vistas/plantilla.php";
+        }
+
+        /*---------- Controlador obtener vistas ----------*/
+        public function obtener_vistas__controlador() {
+            if (isset($_GET['views'])) { // Ver .htaccess, viene por método GET esta variable
+                $ruta = explode("/", $_GET['views']);
+                $respuesta = vistasModelo::obtener_vistas_modelo($ruta[0]);
+
+            } else {
+                $respuesta = "login";
+            }
+            return $respuesta;
+        }
+    }
