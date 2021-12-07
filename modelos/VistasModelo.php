d44db4eb3394fed34bad353519e9f3dbd953af8d
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-07 01:31:57 -0300

    Añadido archivo home-view.php.

--- modelos/VistasModelo.php
+++ modelos/VistasModelo.php
@@ -4,7 +4,7 @@
         
         /*---------- Modelo obtener vistas ----------*/
         protected static function obtener_vistas_modelo($vistas) {
-            $listaBlanca = [];
+            $listaBlanca = ["home"];
             if (in_array($vistas, $listaBlanca)) {
                 if (is_file("./vistas/contenidos".$vistas."-view.php")) {
                     $contenido = "./vistas/contenidos".$vistas."-view.php";
