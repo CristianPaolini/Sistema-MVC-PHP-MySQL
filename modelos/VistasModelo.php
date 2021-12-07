d3d14b094c879f8888162613e8a8095bbb079167
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-07 01:38:12 -0300

    Añadido archivo client-list-view.php.

--- modelos/VistasModelo.php
+++ modelos/VistasModelo.php
@@ -4,7 +4,7 @@
         
         /*---------- Modelo obtener vistas ----------*/
         protected static function obtener_vistas_modelo($vistas) {
-            $listaBlanca = ["home"];
+            $listaBlanca = ["home", "client-list"];
             if (in_array($vistas, $listaBlanca)) {
                 if (is_file("./vistas/contenidos".$vistas."-view.php")) {
                     $contenido = "./vistas/contenidos".$vistas."-view.php";
