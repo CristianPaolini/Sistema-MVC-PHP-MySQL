a280de284c58dbfffded893cad9e54cdec23ce1c
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-07 21:15:57 -0300

    Añadida View item-list.

--- modelos/VistasModelo.php
+++ modelos/VistasModelo.php
@@ -5,7 +5,7 @@
         /*---------- Modelo obtener vistas ----------*/
         protected static function obtener_vistas_modelo($vistas) {
             $listaBlanca = ["home", "client-list", "client-new", "client-search",
-             "client-update", "company"];
+             "client-update", "company", "item-list"];
             if (in_array($vistas, $listaBlanca)) {
                 if (is_file("./vistas/contenidos/".$vistas."-view.php")) {
                     $contenido = "./vistas/contenidos/".$vistas."-view.php";
