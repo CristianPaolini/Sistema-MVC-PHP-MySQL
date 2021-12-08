1add13bbcf2a0f547de2220e6456f3e9407dd91d
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-07 21:18:00 -0300

    Agregada item-new View.

--- modelos/VistasModelo.php
+++ modelos/VistasModelo.php
@@ -5,7 +5,7 @@
         /*---------- Modelo obtener vistas ----------*/
         protected static function obtener_vistas_modelo($vistas) {
             $listaBlanca = ["home", "client-list", "client-new", "client-search",
-             "client-update", "company", "item-list"];
+             "client-update", "company", "item-list", "item-new"];
             if (in_array($vistas, $listaBlanca)) {
                 if (is_file("./vistas/contenidos/".$vistas."-view.php")) {
                     $contenido = "./vistas/contenidos/".$vistas."-view.php";
