d4421862ad1658bfad76faddc55a26bee01618d4
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-07 22:17:45 -0300

    Añadida View reservation-update.

--- modelos/VistasModelo.php
+++ modelos/VistasModelo.php
@@ -7,7 +7,7 @@
             $listaBlanca = ["home", "client-list", "client-new", "client-search",
             "client-update", "company", "item-list", "item-new", "item-search",
             "item-update", "reservation-list", "reservation-new", "reservation-pending",
-            "reservation-reservation", "reservation-search"];
+            "reservation-reservation", "reservation-search", "reservation-update"];
             if (in_array($vistas, $listaBlanca)) {
                 if (is_file("./vistas/contenidos/".$vistas."-view.php")) {
                     $contenido = "./vistas/contenidos/".$vistas."-view.php";
