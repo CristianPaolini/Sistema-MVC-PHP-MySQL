3c56efca47e870bc3d529332fa7804ef908bdebc
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-07 21:04:27 -0300

    Añadida client-search View.

--- modelos/VistasModelo.php
+++ modelos/VistasModelo.php
@@ -4,7 +4,7 @@
         
         /*---------- Modelo obtener vistas ----------*/
         protected static function obtener_vistas_modelo($vistas) {
-            $listaBlanca = ["home", "client-list", "client-new"];
+            $listaBlanca = ["home", "client-list", "client-new", "client-search"];
             if (in_array($vistas, $listaBlanca)) {
                 if (is_file("./vistas/contenidos/".$vistas."-view.php")) {
                     $contenido = "./vistas/contenidos/".$vistas."-view.php";
