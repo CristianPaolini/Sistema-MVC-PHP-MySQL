dd00fa869986b4fdca0183132f3120ac6bb429b5
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-07 21:20:15 -0300

    Añadida item-search View.

--- modelos/VistasModelo.php
+++ modelos/VistasModelo.php
@@ -5,7 +5,7 @@
         /*---------- Modelo obtener vistas ----------*/
         protected static function obtener_vistas_modelo($vistas) {
             $listaBlanca = ["home", "client-list", "client-new", "client-search",
-             "client-update", "company", "item-list", "item-new"];
+             "client-update", "company", "item-list", "item-new", "item-search"];
             if (in_array($vistas, $listaBlanca)) {
                 if (is_file("./vistas/contenidos/".$vistas."-view.php")) {
                     $contenido = "./vistas/contenidos/".$vistas."-view.php";
