3130180c6ff978f348159d71ceaa01a25229acd8
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-07 21:13:30 -0300

    Agregada company View.

--- modelos/VistasModelo.php
+++ modelos/VistasModelo.php
@@ -4,7 +4,8 @@
         
         /*---------- Modelo obtener vistas ----------*/
         protected static function obtener_vistas_modelo($vistas) {
-            $listaBlanca = ["home", "client-list", "client-new", "client-search", "client-update"];
+            $listaBlanca = ["home", "client-list", "client-new", "client-search",
+             "client-update", "company"];
             if (in_array($vistas, $listaBlanca)) {
                 if (is_file("./vistas/contenidos/".$vistas."-view.php")) {
                     $contenido = "./vistas/contenidos/".$vistas."-view.php";
