9748d24a2a5c39d99278b56393ee5f9ff6322f21
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-07 22:12:22 -0300

    Agregada View reservation-reservation, para reservaciones.

--- modelos/VistasModelo.php
+++ modelos/VistasModelo.php
@@ -6,7 +6,8 @@
         protected static function obtener_vistas_modelo($vistas) {
             $listaBlanca = ["home", "client-list", "client-new", "client-search",
             "client-update", "company", "item-list", "item-new", "item-search",
-            "item-update", "reservation-list", "reservation-new", "reservation-pending"];
+            "item-update", "reservation-list", "reservation-new", "reservation-pending",
+            "reservation-reservation"];
             if (in_array($vistas, $listaBlanca)) {
                 if (is_file("./vistas/contenidos/".$vistas."-view.php")) {
                     $contenido = "./vistas/contenidos/".$vistas."-view.php";
