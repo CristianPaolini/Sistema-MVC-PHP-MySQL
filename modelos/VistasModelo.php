c97d18e7de41a408626f1352f06227a6feb70d92
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-11 14:20:52 -0300

    Añadida View user-list.

--- modelos/VistasModelo.php
+++ modelos/VistasModelo.php
@@ -7,7 +7,8 @@
             $listaBlanca = ["home", "client-list", "client-new", "client-search",
             "client-update", "company", "item-list", "item-new", "item-search",
             "item-update", "reservation-list", "reservation-new", "reservation-pending",
-            "reservation-reservation", "reservation-search", "reservation-update"];
+            "reservation-reservation", "reservation-search", "reservation-update",
+            "user-list"];
             if (in_array($vistas, $listaBlanca)) {
                 if (is_file("./vistas/contenidos/".$vistas."-view.php")) {
                     $contenido = "./vistas/contenidos/".$vistas."-view.php";
