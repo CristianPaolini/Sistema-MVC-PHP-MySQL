91c8456c674fc14ddb46c6dd26ad5ae728510217
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-12 09:59:26 -0300

    Modificación de PATHS en la View home.

--- modelos/VistasModelo.php
+++ modelos/VistasModelo.php
@@ -4,8 +4,8 @@
         
         /*---------- Modelo obtener vistas ----------*/
         protected static function obtener_vistas_modelo($vistas) {
-            $listaBlanca = ["home", "client-list", "client-new", "client-search",
-            "client-update", "company", "item-list", "item-new", "item-search",
+            $listaBlanca = ["client-list", "client-new", "client-search",
+            "client-update", "company", "home", "item-list", "item-new", "item-search",
             "item-update", "reservation-list", "reservation-new", "reservation-pending",
             "reservation-reservation", "reservation-search", "reservation-update",
             "user-list", "user-new", "user-search", "user-update"];
