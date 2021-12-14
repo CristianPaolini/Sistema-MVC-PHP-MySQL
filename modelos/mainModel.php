61a3961aeb409b72fe19aa116bfaa6beb4becf0d
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-14 06:48:23 -0300

    Agregado método para ejecutar consultas en el Modelo principal.

--- modelos/mainModel.php
+++ modelos/mainModel.php
@@ -7,10 +7,20 @@
     }
     
     class mainModel {
+
         /*---------- Función conectar a BD ----------*/
         protected static function conectar() {
             $conexion = new PDO(SGBD, USER, PASS);
             $conexion->exec("SET CHARACTER SET utf8");
             return $conexion;
         }
+
+        /*---------- Función ejecutar consultas simples ----------*/
+        protected static function ejecutar_consulta_simple($consulta) {
+            $sql = self::conectar()->prepare($consulta);
+            $sql->execute();
+            return $sql;
+        }
+
+
     }
