2c09079e8832d08ec443713f091338b88580c090
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-14 09:07:12 -0300

    Añadido método limpiar_cadena.

--- modelos/mainModel.php
+++ modelos/mainModel.php
@@ -49,4 +49,36 @@
             return $letra."-".$numero;
         }
 
+        /*---------- Función limpiar cadenas ----------*/
+        protected static function limpiar_cadena($cadena) { //Método que previene inyecciones SQL y otros tipos de ataques
+            $cadena = trim($cadena);
+            $cadena = stripslashes($cadena);
+            $cadena = str_ireplace("<script>", "", $cadena);
+            $cadena = str_ireplace("</script>", "", $cadena);
+            $cadena = str_ireplace("<script src", "", $cadena);
+            $cadena = str_ireplace("<script type=", "", $cadena);
+            $cadena = str_ireplace("SELECT * FROM", "", $cadena);
+            $cadena = str_ireplace("DELETE FROM", "", $cadena);
+            $cadena = str_ireplace("INSERT INTO", "", $cadena);
+            $cadena = str_ireplace("DROP TABLE", "", $cadena);
+            $cadena = str_ireplace("DROP DATABASE", "", $cadena);
+            $cadena = str_ireplace("TRUNCATE TABLE", "", $cadena);
+            $cadena = str_ireplace("SHOW TABLES", "", $cadena);
+            $cadena = str_ireplace("SHOW DATABASES", "", $cadena);
+            $cadena = str_ireplace("<?php", "", $cadena);
+            $cadena = str_ireplace("--", "", $cadena);
+            $cadena = str_ireplace(">", "", $cadena);
+            $cadena = str_ireplace("<", "", $cadena);
+            $cadena = str_ireplace("[", "", $cadena);
+            $cadena = str_ireplace("]", "", $cadena);
+            $cadena = str_ireplace("^", "", $cadena);
+            $cadena = str_ireplace("==", "", $cadena);
+            $cadena = str_ireplace(";", "", $cadena);
+            $cadena = str_ireplace("::", "", $cadena);
+            $cadena = stripslashes($cadena);
+            $cadena = trim($cadena);
+            return $cadena;
+
+        }
+
     }
