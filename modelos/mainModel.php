329f237fc7abff63781df7008546897c38a187ec
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-14 07:39:51 -0300

    Agregados métodos para encriptar y desencriptar cadenas.

--- modelos/mainModel.php
+++ modelos/mainModel.php
@@ -22,5 +22,22 @@
             return $sql;
         }
 
+        /*---------- Encriptar cadenas ----------*/
+        public function encryption($string){ //Public porque va a ser empleada en múltiples Views.
+			$output = FALSE;
+			$key = hash('sha256', SECRET_KEY);
+			$iv = substr(hash('sha256', SECRET_IV), 0, 16);
+			$output = openssl_encrypt($string, METHOD, $key, 0, $iv);
+			$output = base64_encode($output);
+			return $output;
+		}
+
+        /*---------- Desencriptar cadenas ----------*/
+		protected static function decryption($string){
+			$key = hash('sha256', SECRET_KEY);
+			$iv = substr(hash('sha256', SECRET_IV), 0, 16);
+			$output = openssl_decrypt(base64_decode($string), METHOD, $key, 0, $iv);
+			return $output;
+		}
 
     }
