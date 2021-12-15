668f4e2d9fb07b32b29e5957605535437cdc8471
Cristian Paolini <cristianpaolini3@gmail.com>
2021-12-15 06:20:02 -0300

    Agregada función alertas_ajax, encargada de mostrar las alertas.

--- vistas/js/alertas.js
+++ vistas/js/alertas.js
@@ -9,3 +9,37 @@ formularios_ajax.forEach(formularios => {
     formularios.addEventListener("sumbmit", enviar_formulario_ajax);
 });
 
+function alertas_ajax(alerta) {
+    if (alerta.Alerta === "simple") {
+        Swal.fire({
+            title: alerta.Titulo,
+            text: alerta.Texto,
+            type: alerta.Tipo,
+            confirmButtonText: 'Aceptar'
+          });
+    } else if (alerta.Alerta === "recargar") {
+        Swal.fire({
+            title: alerta.Titulo,
+            text: alerta.Texto,
+            type: alerta.Tipo,
+            confirmButtonText: 'Aceptar'
+          }).then((result) => {
+            if (result.value) {
+              location.reload();
+            }
+          });
+    } else if (alerta.Alerta === "limpiar") {
+        Swal.fire({
+            title: alerta.Titulo,
+            text: alerta.Texto,
+            type: alerta.Tipo,
+            confirmButtonText: 'Aceptar'
+          }).then((result) => {
+            if (result.value) {
+              document.querySelector(".FormularioAjax").reset();
+            }
+          });
+    } else if (alerta.Alerta === "redireccionar") {
+        window.location.href = alerta.URL;
+    }
+}
