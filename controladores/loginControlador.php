6d22ca0b086ae7bdab9994094fe501126c2aa572
Cristian Paolini <cristianpaolini3@gmail.com>
2022-01-02 12:39:38 -0300

    agregado controlador cierre de sesión

--- controladores/loginControlador.php
+++ controladores/loginControlador.php
@@ -102,4 +102,28 @@ if ($peticionAjax) {
                 return header("Location: ".SERVERURL."login/");
             }
         } /* Fin controlador */
+
+        /*---------- Controlador cierre de sesión ----------*/
+        public function cierre_sesion_controlador() {
+            session_start(['name'=>'SPM']);
+            $token = mainModel::decryption($_POST['token']);
+            $usuario = mainModel::decryption($_POST['usuario']);
+
+            if ($token == $_SESSION['token_spm'] && $usuario == $_SESSION['usuario_spm']) {
+                session_unset();
+                session_destroy();
+                $alerta = [
+                    "Alerta"=>"redireccionar",
+                    "URL"=>SERVERURL."login/"
+                ];
+            } else {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"No se pudo cerrar la sesión.",
+                    "Tipo"=>"error"
+                ];
+            }
+            echo json_encode($alerta);
+        } /* Fin controlador */
     }
