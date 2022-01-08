17ec0bf17ba73423e4190ef7fcb444dfa56b34cb
Cristian Paolini <cristianpaolini3@gmail.com>
2022-01-08 12:30:52 -0300

    precarga de valores (dni, nombre, apellido, teléfono, dirección) al actualizar usuario

--- vistas/contenidos/user-update-view.php
+++ vistas/contenidos/user-update-view.php
@@ -41,7 +41,9 @@
 		if ($datos_usuario->rowCount() == 1) {
 			$campos = $datos_usuario->fetch();
 	?>
-	<form action="" class="form-neon" autocomplete="off">
+	<form class="form-neon FormularioAjax" action="<?php echo SERVERURL; ?>ajax/usuarioAjax.php" method="POST" data-form="update"
+	 autocomplete="off">
+		<input type="hidden" name="usuario_id_up" value="<?php echo $pagina[1]; ?>">
 		<fieldset>
 			<legend><i class="far fa-address-card"></i> &nbsp; Información personal</legend>
 			<div class="container-fluid">
@@ -49,32 +51,38 @@
 					<div class="col-12 col-md-4">
 						<div class="form-group">
 							<label for="usuario_dni" class="bmd-label-floating">DNI</label>
-							<input type="text" pattern="[0-9-]{1,20}" class="form-control" name="usuario_dni_up" id="usuario_dni" maxlength="20">
+							<input type="text" pattern="[0-9-]{1,20}" class="form-control" name="usuario_dni_up"
+							id="usuario_dni" maxlength="20" value="<?php echo $campos['usuario_dni']; //cargo DNI por default ?>">
 						</div>
 					</div>
 					
 					<div class="col-12 col-md-4">
 						<div class="form-group">
 							<label for="usuario_nombre" class="bmd-label-floating">Nombres</label>
-							<input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" name="usuario_nombre_up" id="usuario_nombre" maxlength="35">
+							<input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" name="usuario_nombre_up"
+							id="usuario_nombre" maxlength="35" value="<?php echo $campos['usuario_nombre']; ?>">
 						</div>
 					</div>
 					<div class="col-12 col-md-4">
 						<div class="form-group">
 							<label for="usuario_apellido" class="bmd-label-floating">Apellidos</label>
-							<input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" name="usuario_apellido_up" id="usuario_apellido" maxlength="35">
+							<input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" name="usuario_apellido_up"
+							id="usuario_apellido" maxlength="35" value="<?php echo $campos['usuario_apellido']; ?>">
 						</div>
 					</div>
 					<div class="col-12 col-md-6">
 						<div class="form-group">
 							<label for="usuario_telefono" class="bmd-label-floating">Teléfono</label>
-							<input type="text" pattern="[0-9()+]{8,20}" class="form-control" name="usuario_telefono_up" id="usuario_telefono" maxlength="20">
+							<input type="text" pattern="[0-9()+]{8,20}" class="form-control" name="usuario_telefono_up"
+							id="usuario_telefono" maxlength="20" value="<?php echo $campos['usuario_telefono']; ?>">
 						</div>
 					</div>
 					<div class="col-12 col-md-6">
 						<div class="form-group">
 							<label for="usuario_direccion" class="bmd-label-floating">Dirección</label>
-							<input type="text" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ().,#\- ]{1,190}" class="form-control" name="usuario_direccion_up" id="usuario_direccion" maxlength="190">
+							<input type="text" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ().,#\- ]{1,190}" class="form-control"
+							name="usuario_direccion_up" id="usuario_direccion" maxlength="190"
+							value="<?php echo $campos['usuario_direccion']; ?>">
 						</div>
 					</div>
 				</div>
