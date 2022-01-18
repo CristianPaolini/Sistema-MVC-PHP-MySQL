d91dd3797b5567d3be4e8735b54505b3511831fa
Cristian Paolini <cristianpaolini3@gmail.com>
2022-01-18 12:15:53 -0300

    inicio funcionalidad del controlador agregar usuario

--- controladores/clienteControlador.php
+++ controladores/clienteControlador.php
@@ -10,6 +10,24 @@
         
         /*---------- Controlador agregar cliente ----------*/
         public function agregar_cliente_controlador() {
-    
+            $dni = mainModel::limpiar_cadena($_POST['cliente_dni_reg']);
+            $nombre = mainModel::limpiar_cadena($_POST['cliente_nombre_reg']);
+            $apellido = mainModel::limpiar_cadena($_POST['cliente_apellido_reg']);
+            $telefono = mainModel::limpiar_cadena($_POST['cliente_telefono_reg']);
+            $direccion = mainModel::limpiar_cadena($_POST['cliente_direccion_reg']);
+
+            /*== Comprobar campos vacíos ==*/
+            if ($dni == "" || $nombre == "" || $apellido == "" || $telefono == "" ||
+             $direccion == "") {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"No ha completado todos los campos obligatorios.",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+
         } /* Fin controlador */
     }
