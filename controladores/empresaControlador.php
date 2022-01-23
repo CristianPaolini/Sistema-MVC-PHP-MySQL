ec9a92aa28762482953b308976cce82d99c5beb0
Cristian Paolini <cristianpaolini3@gmail.com>
2022-01-23 00:13:11 -0300

    agregadas validaciones en controlador agregar empresa

--- controladores/empresaControlador.php
+++ controladores/empresaControlador.php
@@ -15,6 +15,67 @@
 
         /*---------- Controlador agregar empresa ----------*/
         public function agregar_empresa_controlador() {
+            $nombre = mainModel::limpiar_cadena($_POST['empresa_nombre_reg']);
+            $email = mainModel::limpiar_cadena($_POST['empresa_email_reg']);
+            $telefono = mainModel::limpiar_cadena($_POST['empresa_telefono_reg']);
+            $direccion = mainModel::limpiar_cadena($_POST['empresa_direccion_reg']);
 
+            /*== Comprobar campos vacíos ==*/
+            if ($nombre == "" || $email == "" || $telefono == "" ||
+             $direccion == "") {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"No ha completado todos los campos obligatorios.",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+
+            /*== Verificar integridad de los datos ==*/
+            if (mainModel::verificar_datos("[a-zA-z0-9áéíóúÁÉÍÓÚñÑ. ]{1,70}", $nombre)) {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"El formato de NOMBRE DE EMPRESA no es válido.",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+            
+            if (mainModel::verificar_datos("[0-9()+]{8,20}", $telefono)) {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"El formato de TELÉFONO no es válido.",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+
+            if (mainModel::verificar_datos("[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ().,#\- ]{1,190}", $direccion)) {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"El formato de DIRECCIÓN no es válido.",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"El formato de EMAIL no es válido.",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
         } /* Fin controlador */
     }
