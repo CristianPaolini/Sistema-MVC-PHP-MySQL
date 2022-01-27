5a2c2524780de129d5f648481ae1b7353669b462
Cristian Paolini <cristianpaolini3@gmail.com>
2022-01-27 16:56:05 -0300

    añadida funcionalidad correspondiente al controlador actualizar empresa

--- controladores/empresaControlador.php
+++ controladores/empresaControlador.php
@@ -95,7 +95,7 @@
                 "Nombre"=>$nombre,
                 "Email"=>$email,
                 "Telefono"=>$telefono,
-                "Direccion"=>$direccion,
+                "Direccion"=>$direccion
             ];
 
             $agregar_empresa = empresaModelo::agregar_empresa_modelo($datos_empresa_reg);
@@ -119,6 +119,106 @@
 
         /*---------- Controlador actualizar empresa ----------*/
         public function actualizar_empresa_controlador() {
+            $id = mainModel::limpiar_cadena($_POST['empresa_id_up']);
+            $nombre = mainModel::limpiar_cadena($_POST['empresa_nombre_up']);
+            $email = mainModel::limpiar_cadena($_POST['empresa_email_up']);
+            $telefono = mainModel::limpiar_cadena($_POST['empresa_telefono_up']);
+            $direccion = mainModel::limpiar_cadena($_POST['empresa_direccion_up']);
 
+            /*== Comprobar campos vacíos ==*/
+            if ($nombre == "" || $email == "" || $telefono == "" ||
+            $direccion == "") {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"No ha completado todos los campos obligatorios.",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+
+            /*== Verificar integridad de los datos ==*/
+            if (mainModel::verificar_datos("[a-zA-z0-9áéíóúÁÉÍÓÚñÑ. ]{1,70}", $nombre)) {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"El formato de NOMBRE DE EMPRESA no es válido.",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+
+            if (mainModel::verificar_datos("[0-9()+]{8,20}", $telefono)) {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"El formato de TELÉFONO no es válido.",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+
+            if (mainModel::verificar_datos("[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ().,#\- ]{1,190}", $direccion)) {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"El formato de DIRECCIÓN no es válido.",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"El formato de EMAIL no es válido.",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+
+            /*== Comprobar privilegios ==*/
+            session_start(['name'=>'SPM']);
+            if ($_SESSION['privilegio_spm'] < 1 || $_SESSION['privilegio_spm'] > 2) {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"No tiene los permisos necesarios para ejecutar la operación.",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+
+            $datos_empresa_up = [
+                "ID"=>$id,
+                "Nombre"=>$nombre,
+                "Email"=>$email,
+                "Telefono"=>$telefono,
+                "Direccion"=>$direccion
+            ];
+
+            if (empresaModelo::actualizar_empresa_modelo($datos_empresa_up)) {
+                $alerta = [
+                    "Alerta"=>"recargar",
+                    "Titulo"=>"Datos actualizados",
+                    "Texto"=>"Los datos de la empresa han sido actualizados con éxito.",
+                    "Tipo"=>"success"
+                ];
+            } else {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"No se pudo actualizar los datos de la empresa. Por favor, intente nuevamente.",
+                    "Tipo"=>"error"
+                ];
+            }
+            echo json_encode($alerta);
         } /* Fin controlador */
     }
