292fdd45be5ce6dbe5c06452a29158a234d0f489
Cristian Paolini <cristianpaolini3@gmail.com>
2022-01-28 14:20:22 -0300

    inicio funcionalidad controlador agregar item. Recuperación de datos de la view item new y comprobación de campos vacíos

--- controladores/itemControlador.php
+++ controladores/itemControlador.php
@@ -10,6 +10,23 @@
         
         /*---------- Controlador agregar item ----------*/
         public function agregar_item_controlador() {
+            $codigo = mainModel::limpiar_cadena($_POST['item_codigo_reg']);
+            $nombre = mainModel::limpiar_cadena($_POST['item_nombre_reg']);
+            $stock = mainModel::limpiar_cadena($_POST['item_stock_reg']);
+            $estado = mainModel::limpiar_cadena($_POST['item_estado_reg']);
+            $detalle = mainModel::limpiar_cadena($_POST['item_detalle_reg']);
 
+            /*== Comprobar campos vacíos ==*/
+            if ($codigo == "" || $nombre == "" || $stock == "" ||
+             $estado == "") {
+                $alerta = [
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrió un error inesperado",
+                    "Texto"=>"No ha completado todos los campos obligatorios.",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
         } /* Fin controlador */
     }
