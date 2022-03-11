06c567df7a41ea8cbb5bc7ceb250e3bfcde7a99c
Cristian Paolini <cristianpaolini3@gmail.com>
2022-03-11 19:24:59 -0300

    agregados valores de BD relacionados al préstamo para generar la factura

--- facturas/invoice.php
+++ facturas/invoice.php
@@ -14,6 +14,26 @@
 		if ($datos_prestamo->rowCount() == 1) {
 			$datos_prestamo = $datos_prestamo->fetch();
 
+	/*---------- Instancia al controlador empresa ----------*/
+		require_once "../controladores/empresaControlador.php";
+		$ins_empresa = new empresaControlador();
+
+		$datos_empresa = $ins_empresa->datos_empresa_controlador();
+		$datos_empresa = $datos_empresa->fetch();
+
+		/*---------- Instancia al controlador usuario ----------*/
+		require_once "../controladores/usuarioControlador.php";
+		$ins_usuario = new usuarioControlador();
+
+		$datos_usuario = $ins_usuario->datos_usuario_controlador("Unico", $ins_usuario->encryption($datos_prestamo['usuario_id']));
+		$datos_usuario = $datos_usuario->fetch();
+
+		/*---------- Instancia al controlador cliente ----------*/
+		require_once "../controladores/clienteControlador.php";
+		$ins_cliente = new clienteControlador();
+
+		$datos_cliente = $ins_cliente->datos_cliente_controlador("Unico", $ins_cliente->encryption($datos_prestamo['cliente_id']));
+		$datos_cliente = $datos_cliente->fetch();
 	require "./fpdf.php";
 
 	$pdf = new FPDF('P','mm','Letter');
@@ -23,7 +43,7 @@
 
 	$pdf->SetFont('Arial','B',18);
 	$pdf->SetTextColor(0,107,181);
-	$pdf->Cell(0,10,utf8_decode(strtoupper("nombre de la empresa")),0,0,'C');
+	$pdf->Cell(0,10,utf8_decode(strtoupper($datos_empresa['empresa_nombre'])),0,0,'C');
 	$pdf->SetFont('Arial','',12);
 	$pdf->SetTextColor(33,33,33);
 	$pdf->Cell(-35,10,utf8_decode('N. de factura'),'',0,'C');
@@ -35,19 +55,19 @@
 	$pdf->Cell(0,10,utf8_decode(""),0,0,'C');
 	$pdf->SetFont('Arial','',12);
 	$pdf->SetTextColor(97,97,97);
-	$pdf->Cell(-35,10,utf8_decode("CODIGO DE FACTURA"),'',0,'C');
+	$pdf->Cell(-35,10,utf8_decode($datos_prestamo['prestamo_id']),'',0,'C');
 
 	$pdf->Ln(25);
 
 	$pdf->SetTextColor(33,33,33);
 	$pdf->Cell(36,8,utf8_decode('Fecha de emisión:'),0,0);
 	$pdf->SetTextColor(97,97,97);
-	$pdf->Cell(27,8,utf8_decode(date("d/m/Y", strtotime("2020-01-07"))),0,0);
+	$pdf->Cell(27,8,utf8_decode(date("d/m/Y", strtotime($datos_prestamo['prestamo_fecha_inicio']))),0,0);
 	$pdf->Ln(8);
 	$pdf->SetTextColor(33,33,33);
 	$pdf->Cell(27,8,utf8_decode('Atendido por:'),"",0,0);
 	$pdf->SetTextColor(97,97,97);
-	$pdf->Cell(13,8,utf8_decode("NOMBRE DEL ADMINISTRADOR"),0,0);
+	$pdf->Cell(13,8,utf8_decode($datos_usuario['usuario_nombre']." ".$datos_usuario['usuario_apellido']),0,0);
 
 	$pdf->Ln(15);
 
@@ -55,22 +75,22 @@
 	$pdf->SetTextColor(33,33,33);
 	$pdf->Cell(15,8,utf8_decode('Cliente:'),0,0);
 	$pdf->SetTextColor(97,97,97);
-	$pdf->Cell(65,8,utf8_decode("NOMBRE DEL CLIENTE"),0,0);
+	$pdf->Cell(65,8,utf8_decode($datos_cliente['cliente_nombre']." ".$datos_cliente['cliente_apellido']),0,0);
 	$pdf->SetTextColor(33,33,33);
 	$pdf->Cell(10,8,utf8_decode('DNI:'),0,0);
 	$pdf->SetTextColor(97,97,97);
-	$pdf->Cell(40,8,utf8_decode("0000000000"),0,0);
+	$pdf->Cell(40,8,utf8_decode($datos_cliente['cliente_dni']),0,0);
 	$pdf->SetTextColor(33,33,33);
 	$pdf->Cell(19,8,utf8_decode('Teléfono:'),0,0);
 	$pdf->SetTextColor(97,97,97);
-	$pdf->Cell(35,8,utf8_decode("(000)00000000"),0,0);
+	$pdf->Cell(35,8,utf8_decode($datos_cliente['cliente_telefono']),0,0);
 	$pdf->SetTextColor(33,33,33);
 
 	$pdf->Ln(8);
 
 	$pdf->Cell(8,8,utf8_decode('Dir:'),0,0);
 	$pdf->SetTextColor(97,97,97);
-	$pdf->Cell(109,8,utf8_decode("DIRECCION DEL CLIENTE"),0,0);
+	$pdf->Cell(109,8,utf8_decode($datos_cliente['cliente_direccion']),0,0);
 
 	$pdf->Ln(15);
 
